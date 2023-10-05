edf0e510d448c864fc74b5c5e67b82c3b44cb8ff
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-05 15:51:18 -0600

    moved the getJokesBySearch function to JokesSearchViewModel and made it a private method

--- HW4-ChuckNorris-JokesApp/Model/SearchModel.swift
+++ HW4-ChuckNorris-JokesApp/Model/SearchModel.swift
@@ -4,7 +4,6 @@
 //
 //  Created by octopus on 9/28/23.
 //
-import SwiftUI
 
 struct JokesResponse: Decodable {
     let total: Int
@@ -24,15 +23,5 @@ struct JokesResults: Decodable, Identifiable {
     init(){
         self.id = "123"
         self.value = "Search for a Joke"
-    }
-}
-
-func getJokesBySearch(search: String = "") async -> JokesResponse? {
-    do {
-        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/search?query=\(search)")!)
-        let jokesResults = try JSONDecoder().decode(JokesResponse.self, from: data)
-        return jokesResults
-    } catch {
-        return nil
     }
 }
--- HW4-ChuckNorris-JokesApp/ViewModel/JokesSearchViewModel.swift
+++ HW4-ChuckNorris-JokesApp/ViewModel/JokesSearchViewModel.swift
@@ -12,6 +12,16 @@ class JokesSearchViewModel: ObservableObject {
     @Published var results = JokesResponse()
     
     func getJokes() async{
-        self.results = await getJokesBySearch(search: self.search) ?? JokesResponse()
+        self.results = await self.getJokesBySearch(search: self.search) ?? JokesResponse()
+    }
+    
+    private func getJokesBySearch(search: String = "") async -> JokesResponse? {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/search?query=\(search)")!)
+            let jokesResults = try JSONDecoder().decode(JokesResponse.self, from: data)
+            return jokesResults
+        } catch {
+            return nil
+        }
     }
 }
