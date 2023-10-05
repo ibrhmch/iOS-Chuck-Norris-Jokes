a5fe624645a045bb6804b5fe2bb64d4a07342333
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-05 15:12:17 -0600

    removed the getRandomJoke function

--- HW4-ChuckNorris-JokesApp/Model/JokeModel.swift
+++ HW4-ChuckNorris-JokesApp/Model/JokeModel.swift
@@ -6,14 +6,3 @@ struct Joke: Codable {
     let url: String
     let value: String
 }
-
-
-func getRandomJoke(category: String = "travel") async -> String? {
-    do {
-        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/random?category=\(category)")!)
-        let joke = try JSONDecoder().decode(Joke.self, from: data)
-        return joke.value
-    } catch {
-        return nil
-    }
-}
