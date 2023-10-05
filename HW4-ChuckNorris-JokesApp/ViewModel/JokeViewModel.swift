f0160c8c9e3e60b53815b5435552081a682347b5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-05 15:13:05 -0600

    add the getRandomJoke methd

--- HW4-ChuckNorris-JokesApp/ViewModel/JokeViewModel.swift
+++ HW4-ChuckNorris-JokesApp/ViewModel/JokeViewModel.swift
@@ -6,4 +6,14 @@ class JokeViewModel: ObservableObject {
     func fetchJoke(category: String) async {
         joke = await getRandomJoke(category: category) ?? ""
     }
+    
+    func getRandomJoke(category: String = "travel") async -> String? {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/random?category=\(category)")!)
+            let joke = try JSONDecoder().decode(Joke.self, from: data)
+            return joke.value
+        } catch {
+            return nil
+        }
+    }
 }
