02d859c88c267667bd0c8a6d8d5e0c4fa75fef6b
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-05 21:05:44 -0600

    moved the settings tab logic

--- HW4-ChuckNorris-JokesApp/View/SettingsView.swift
+++ HW4-ChuckNorris-JokesApp/View/SettingsView.swift
@@ -7,12 +7,85 @@
 
 import SwiftUI
 
-struct SettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Settings: View {
+    @Binding var backgroundColor: Color
+    @Binding var explicitAllowed: Bool
+    @State var showingSheet = false
+    
+    var body: some View{
+        NavigationStack{
+            VStack{
+                NavigationLink("Customize the App") {
+                    DetailView(backgroundColor: $backgroundColor,
+                               explicitAllowed: $explicitAllowed)
+                }
+                .frame(width: 400, height: 50)
+                .background(Color.black)
+                .foregroundColor(Color.white)
+                
+                Button("Reset the color"){
+                    showingSheet = true
+                }
+                .frame(width: 400, height: 50)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+                .sheet (isPresented: $showingSheet) {
+                    SheetView(backgroundColor: $backgroundColor)
+                }
+                
+            }
+            .position(x:180, y:100)
+            .padding()
+            .navigationTitle("Settings")
+            .background(backgroundColor)
+        }
     }
 }
 
-#Preview {
-    SettingsView()
+struct DetailView: View {
+    @Binding var backgroundColor: Color
+    @Binding var explicitAllowed: Bool
+    
+    var body: some View {
+        VStack{
+            Toggle("Explicit Category", isOn: $explicitAllowed)
+            
+            Button("Randomize the background color ↝"){
+                backgroundColor = randomColor()
+                // Call the function using an async task
+            }
+            .padding(20)
+            .background(Color.indigo)
+            .foregroundColor(Color.white)
+            .cornerRadius(7)
+            .overlay(
+                RoundedRectangle(cornerRadius: 7)
+                    .stroke(Color.black, lineWidth: 1)
+                )
+            .offset(y:100)
+        }
+        .padding()
+        .frame(width: 400, height: 700)
+        .background(backgroundColor)
+    }
+}
+
+struct SheetView: View {
+    @Environment (\.dismiss) var dismiss
+    @Binding var backgroundColor: Color
+    var body: some View {
+        Button ("Press to Reset Color") {
+            backgroundColor = Color.white
+            dismiss()
+        }
+        .padding(20)
+        .background(Color.indigo)
+        .foregroundColor(Color.white)
+        .cornerRadius(7)
+        .overlay(
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(Color.black, lineWidth: 1)
+            )
+        .offset(y:100)
+    }
 }
