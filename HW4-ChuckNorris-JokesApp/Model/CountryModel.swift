4b05e03e69b1a702e567831a9d1d667e6bff61f6
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-12 22:56:52 -0600

    created country model to fetch country name and coordinates

--- HW4-ChuckNorris-JokesApp/Model/CountryModel.swift
+++ HW4-ChuckNorris-JokesApp/Model/CountryModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import MapKit
+
+struct Country: Codable, Identifiable{
+    var id: Int { return UUID().hashValue }
+    var name: CountryName
+    var latlng: Array<Double>?
+    
+    var coordinate: CLLocationCoordinate2D? {
+        guard let latlng, latlng.count >= 2 else { return nil }
+        return CLLocationCoordinate2D(latitude: latlng[0], longitude: latlng[1])
+    }
+    
+    init(){
+        self.name = CountryName()
+    }
+    
+}
+
+struct CountryName: Codable{
+    var common: String
+    var official: String
+    
+    init(){
+        self.common = "temp"
+        self.official = "temp"
+    }
+    
+}
